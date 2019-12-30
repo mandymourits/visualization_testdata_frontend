ba753b099df5dbfe7d5fe59fc9f85f6318f8feed
Harm <h.bruins@ventus.nl>
2019-12-30 01:04:23 +0100

    no message

---
+++ docs/assets/precache-manifest.05da27222a2cd4c7bbb5f0459f517e60.js
@@ -0,0 +1,48 @@
+self.__precacheManifest = [
+  {
+    "url": "/assets/7.505eb9c29d7c4c5cf99d.js"
+  },
+  {
+    "url": "/assets/vendors.505eb9c29d7c4c5cf99d.js"
+  },
+  {
+    "revision": "8a9c815de79fa79f7a9a66a19a34b2c0",
+    "url": "/assets/ventus.jpg"
+  },
+  {
+    "url": "/assets/8.505eb9c29d7c4c5cf99d.js"
+  },
+  {
+    "url": "/assets/styles.505eb9c29d7c4c5cf99d.js"
+  },
+  {
+    "revision": "9917a82bb1de4a6507cd0771c92ae556",
+    "url": "/assets/targets.png"
+  },
+  {
+    "revision": "5dc27cfe227ea3fc5f89a6c9c9a620c8",
+    "url": "/assets/modernizr-bundle.js"
+  },
+  {
+    "url": "/assets/app.505eb9c29d7c4c5cf99d.js"
+  },
+  {
+    "url": "/assets/9.505eb9c29d7c4c5cf99d.js"
+  },
+  {
+    "url": "/assets/6.505eb9c29d7c4c5cf99d.js"
+  },
+  {
+    "url": "/assets/5.505eb9c29d7c4c5cf99d.js"
+  },
+  {
+    "url": "/assets/4.505eb9c29d7c4c5cf99d.js"
+  },
+  {
+    "url": "/assets/1.505eb9c29d7c4c5cf99d.js"
+  },
+  {
+    "revision": "8e0099fa708687b8f24bb2dd4cd6399d",
+    "url": "/assets/../index.html"
+  }
+];
